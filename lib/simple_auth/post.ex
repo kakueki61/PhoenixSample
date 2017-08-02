5d892443aef57b2db7aa7d94a2673f0624d87c79
kakueki61 <t.kodama61@gmail.com>
2017-08-03 01:02:19 +0900

    post and user association

---
+++ lib/simple_auth/post.ex
@@ -0,0 +1,21 @@
+defmodule SimpleAuth.Post do
+  use Ecto.Schema
+  @moduledoc false
+
+  schema "posts" do
+    belongs_to :user, SimpleAuth.User
+
+    field :title, :string
+    field :body, :string
+  end
+
+  @required_fields ~w(title)a
+  @optional_fields ~w(body)a
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> assoc_constraint(:user)
+  end
+end
--- lib/simple_auth/user.ex
+++ lib/simple_auth/user.ex
@@ -5,16 +5,24 @@ defmodule SimpleAuth.User do
 
 
   schema "users" do
+    has_many :posts, SimpleAuth.Post
+
     field :email, :string
     field :name, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :is_admin, :boolean, default: false
 
     timestamps()
   end
 
+  @required_fields ~w(email)a
+  @optional_fields ~w(name is_admin)a
+
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:email, :name])
-    |> validate_required([:email, :name])
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
   end
 end

cbc0cee50f3d71f43b6e04bdc7dd8908ef983c10
kakueki61 <t.kodama61@gmail.com>
2017-08-17 00:05:29 +0900

    Refactors with Auth

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -2,10 +2,6 @@ defmodule SimpleAuth.SessionController do
   @moduledoc false
   use SimpleAuth.Web, :controller
 
-  import Comeonin.Pbkdf2, only: [checkpw: 2, dummy_checkpw: 0]
-
-  alias SimpleAuth.User
-
   plug :scrub_params, "session" when action in ~w(create)a
 
   def new(conn, _) do
@@ -13,49 +9,22 @@ defmodule SimpleAuth.SessionController do
   end
 
   def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
-    # try to get user by unique email from DB
-    user = Repo.get_by(User, email: email)
-
-    # examine the result
-    result = cond do
-      # if user was found and provided password hash equals to stored hash
-      user && checkpw(password, user.password_hash) ->
-        { :ok, login(conn, user) }
-      # else if we just found the user
-      user ->
-        { :error, :unauthorized, conn }
-      # otherwise
-      true ->
-        # simulate check password hash timing
-        dummy_checkpw
-        { :error, :not_found, conn }
-    end
-
-    case result do
+    case SimpleAuth.Auth.login_by_email_and_pass(conn, email, password) do
       { :ok, conn } ->
         conn
-        |> put_flash(:info, "You're now logged in!")
+        |> put_flash(:info, "You're now signed in!'")
         |> redirect(to: page_path(conn, :index))
       { :error, _reason, conn } ->
         conn
         |> put_flash(:error, "Invalid email/password combination")
-        |> render(:new)
+        |> render("new.html")
     end
-  end
-
-  defp login(conn, user) do
-    conn
-    |> Guardian.Plug.sign_in(user)
   end
 
   def delete(conn, _) do
     conn
-    |> logout
-    |> put_flash(:info, "See your later!")
+    |> SimpleAuth.Auth.logout
+    |> put_flash(:info, "See you later!")
     |> redirect(to: page_path(conn, :index))
-  end
-
-  defp logout(conn) do
-    Guardian.Plug.sign_out(conn)
   end
 end
